faacd15f9e4b0172f3958157b57c1ee733c98961
Courteney Ervin <courteney.ervin@gmail.com>
2016-04-27 17:48:34 -0400

    The meat & potatoes of the functionality: randomness, files, a winning personality

---
+++ index.rb
@@ -0,0 +1,24 @@
+require 'tilt/erubis'
+require 'sinatra'
+require 'pry'
+​
+set :views, settings.root
+​
+def get_random_file(dir)
+  filenames = []
+  Dir.new(settings.root + dir).each { |f| filenames << f }
+  filenames.delete('.')
+  filenames.delete('..')
+  filenames.sample
+end
+​
+get '/' do
+  redirect '/vows'
+end
+​
+get '/vows' do
+  text_file = get_random_file('/vows')
+  @text = File.open(settings.root + '/vows/' + text_file).readlines("\n")
+  @css_path = 'css/' + get_random_file('/public/css')
+  erb :index
+end
